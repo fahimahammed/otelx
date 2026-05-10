bbddeaed2e5b40e2cbca43aa16a03129c65b08af
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2026-05-11 00:06:33 +0600

    feat: update Docker configuration and add Tempo datasource for Grafana

--- user-service/command.sh
+++ user-service/command.sh
@@ -1,9 +1,9 @@
 docker-compose up -d && docker-compose logs -f
-
+docker compose build --no-cache
 
 curl -X POST http://localhost:3000/api/users \
   -H "Content-Type: application/json" \
   -d '{
-    "name": "Bob",
-    "email": "bob@example.com"
+    "name": "Bob3",
+    "email": "bob37@example.com"
   }'
